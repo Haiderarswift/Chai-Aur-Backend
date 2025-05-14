56a75d6c1b88b271d183dcbab451c2ddb07aa743
unknown <haider@arswift.com>
2025-05-14 16:22:25 +0500

    remove error

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-import dotenv from 'dotenv';
-import express from 'express';
+import express from "express";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import cors from 'cors';
-const app = express();
-app.use(bodyParser.json());
-app.use(express.json());
-app.use(cors());
-app.use(cookieParser());
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
 dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.use(bodyParser.json());
 
-app.get('/login', (req, res) => {
-  res.send('Pleases login yourself');
-});
+app.use(express.json());
+app.use(cors());
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
-});
+app.use(cookieParser());
+
+// mongoose
+//   .connect(process.env.DATABASE_URL)
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => {
+//     console.error("MongoDB Error:", err.message);
+//     process.exit(1); 
+//   });
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
