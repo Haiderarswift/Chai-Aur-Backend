b4df03abcd665396de4536e211001bcf36f8e191
unknown <haider@arswift.com>
2025-05-14 15:43:21 +0500

    deplyment in server

---
+++ server.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/login', (req, res) => {
+  res.send('Pleases login yourself');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
