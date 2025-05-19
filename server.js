3895a819b06aeddcb118466490dfa316a7f0a969
unknown <haider@arswift.com>
2025-05-19 09:24:34 +0500

    remove error

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import mongoose from "mongoose";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = 8000;
 app.use(bodyParser.json());
 
 app.use(express.json());
